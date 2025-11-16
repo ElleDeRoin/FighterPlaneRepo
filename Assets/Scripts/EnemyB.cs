5f0f379a62b56dc9e28f81636a95f9e2403ebb17
ElleDeRoin <ellederoin@gmail.com>
2025-11-16 08:51:25 -0500

    Edit

--- Assets/Scripts/EnemyB.cs
+++ Assets/Scripts/EnemyB.cs
@@ -1,38 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyB : MonoBehaviour
 {
-    public GameObject explosionPrefab;
-    private GameManager gameManager;
+    // Start is called before the first frame update
     void Start()
     {
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-    }
-      // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(new Vector3(0,-1,0) * Time.deltaTime * 8f);
-        if(transform.position.y < -6.5f)
-        {
-            Destroy(this.gameObject);
-        }
+        
     }
 
-    private void OnTriggerEnter(Collider whatDidIHit)
+    // Update is called once per frame
+    void Update()
     {
-        if (whatDidIHit.gameObject.tag == "Player")
+        transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3f, Space.World);
+        if (transform.position.y < -6.5f)
         {
-            whatDidIHit.GetComponent<Player>().LoseALife();
-            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
             Destroy(this.gameObject);
-        }
-        else if (whatDidIHit.gameObject.tag == "Weapons")
-        {
-           Destroy(whatDidIHit.gameObject);
-           Instantiate(explosionPrefab, transform.position, Quaternion.identity);
-           gameManager.AddScore(5);
-           Destroy(this.gameObject);
-           
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
 {
 
     public GameObject playerPrefab;
+    public GameObject enemyTwoPrefab;
     public GameObject enemyOnePrefab;
     public GameObject cloudPrefab;
 
@@ -36,41 +37,45 @@ public class GameManager : MonoBehaviour
         
     }
 
-    void CreateEnemy()
+
+
+void CreateEnemy()
+{
+    float spawnY = verticalScreenSize * 0.9f; // spawn near top
+
+    // Decide which enemy to spawn (50% chance)
+    GameObject enemyToSpawn = (Random.value < 0.5f) ? enemyOnePrefab : enemyTwoPrefab;
+
+    // Get half-size of prefab
+    float halfWidth = 0.5f;
+    float halfHeight = 0.5f;
+
+    Renderer rend = enemyToSpawn.GetComponent<Renderer>();
+    if (rend != null)
     {
-        float spawnY = verticalScreenSize * 0.9f; // spawn near top
-
-        // Get half-size of prefab
-        float halfWidth = 0.5f;
-        float halfHeight = 0.5f;
-
-        Renderer rend = enemyOnePrefab.GetComponent<Renderer>();
-        if (rend != null)
-        {
-            Vector3 size = rend.bounds.size;
-            halfWidth = 0.5f * enemyOnePrefab.transform.localScale.x;
-            halfHeight = 0.5f * enemyOnePrefab.transform.localScale.y;
-        }
-
-        int maxAttempts = 10; // avoid infinite loops
-        for (int i = 0; i < maxAttempts; i++)
-        {
-            // Pick random X inside camera bounds, accounting for prefab width
-            float spawnX = Random.Range(-horizontalScreenSize + halfWidth, horizontalScreenSize - halfWidth);
-            Vector3 spawnPos = new Vector3(spawnX, spawnY, 0f);
-
-            // Check for collisions using the prefab size
-            Collider[] hits = Physics.OverlapBox(spawnPos, new Vector3(halfWidth, halfHeight, 0.5f));
-            if (hits.Length == 0)
-            {
-                Instantiate(enemyOnePrefab, spawnPos, Quaternion.Euler(180, 0, 0));
-                return; // successfully spawned
-            }
-        }
-
-        Debug.Log("Could not find a free spot for enemy this cycle.");
+        Vector3 size = rend.bounds.size;
+        halfWidth = 0.5f * enemyToSpawn.transform.localScale.x;
+        halfHeight = 0.5f * enemyToSpawn.transform.localScale.y;
     }
-    
+
+    int maxAttempts = 10; // avoid infinite loops
+    for (int i = 0; i < maxAttempts; i++)
+    {
+        // Pick random X inside camera bounds, accounting for prefab width
+        float spawnX = Random.Range(-horizontalScreenSize + halfWidth, horizontalScreenSize - halfWidth);
+        Vector3 spawnPos = new Vector3(spawnX, spawnY, 0f);
+
+        // Check for collisions using the prefab size
+        Collider[] hits = Physics.OverlapBox(spawnPos, new Vector3(halfWidth, halfHeight, 0.5f));
+        if (hits.Length == 0)
+        {
+            Instantiate(enemyToSpawn, spawnPos, Quaternion.Euler(180, 0, 0));
+            return; // successfully spawned
+        }
+    }
+
+    Debug.Log("Could not find a free spot for enemy this cycle.");
+}
 
     void CreateSky()
     {
